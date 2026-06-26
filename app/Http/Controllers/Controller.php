bc9309b6910858e49228c3eba57e105bdab3a565
Dmytro Sokol <dfalcon.7c8@gmail.com>
2026-06-26 21:20:28 +0000

    add swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use OpenApi\Attributes as OA;
+
+#[OA\Info(
+    version: '1.0.0',
+    title: 'IntelliBase API',
+)]
+#[OA\SecurityScheme(
+    securityScheme: 'bearerAuth',
+    type: 'http',
+    scheme: 'bearer',
+)]
 abstract class Controller
 {
-    //
 }
